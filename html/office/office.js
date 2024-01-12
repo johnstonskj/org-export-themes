af6fcda387cced4e85887dd34f5c2f6789fed184
Simon Johnston <johnstonskj@gmail.com>
2024-01-12 11:45:52 -0800

    fix: allow for pre-defined KATEX_MACROS

--- html/office/office.js
+++ html/office/office.js
@@ -1,10 +1,8 @@
 function initMath() {
+    const KATEX_MACROS = KATEX_MACROS || {};
     document.addEventListener("DOMContentLoaded", function() {
         renderMathInElement(document.body, {
-            macros: {
-                "\\Saft": "\\operatorname{\\blacksquare}",
-                "\\Sbef": "\\operatorname{\\square}"
-            }
+            macros: KATEX_MACROS
         });
     });
 }
