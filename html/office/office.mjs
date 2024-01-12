c4bc86a41ae864cfce934f025130bbfd9e6797b3
Simon Johnston <johnstonskj@gmail.com>
2024-01-12 13:00:26 -0800

    fix: modularize JS

--- html/office/office.mjs
+++ html/office/office.mjs
@@ -1,6 +1,6 @@
 "use strict";
 
-import renderMathInElement from 'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.mjs';
+import renderMathInElement from 'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/contrib/auto-render.min.mjs';
 
 export default function initMath(options) {
     const KATEX_MACROS = document.KATEX_MACROS || {};
