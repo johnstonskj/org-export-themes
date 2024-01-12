58946404be4d52873434b0e9989239348065e56c
Simon Johnston <johnstonskj@gmail.com>
2024-01-12 12:48:29 -0800

    fix: modularize JS

--- html/office/office.mjs
+++ html/office/office.mjs
@@ -2,7 +2,7 @@
 
 import katex from 'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.mjs';
 
-function initMath(options) {
+export default function initMath(options) {
     const KATEX_MACROS = document.KATEX_MACROS || {};
     document.addEventListener("DOMContentLoaded", function() {
         katex.renderMathInElement(document.body, {
